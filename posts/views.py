39bc4921a570b5fa64cf45463f89751f05663a4f
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-01-15 16:55:26 +0530

    deleting posts via api

--- posts/views.py
+++ posts/views.py
@@ -14,6 +14,19 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
 
+
+class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def delete(self,request, *args,**kwargs):
+        post=Post.objects.filter(pk=kwargs['pk'],poster=self.request.user)
+        if post.exists():
+            return self.destroy(request,*args,**kwargs)
+        else:
+            raise ValidationError('This isnt your post to delete.')
+
+
 class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
     queryset = Post.objects.all()
     serializer_class = VoteSerializer
