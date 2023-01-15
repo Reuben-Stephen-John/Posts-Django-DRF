0cd158a04e4648c18231ab79b227e8df8dfd4a23
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-01-15 12:34:21 +0530

    first commit

---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
+# Create your views here.
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes=[permissions.IsAuthenticated]
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
